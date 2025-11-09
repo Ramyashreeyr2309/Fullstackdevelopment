4a926c9420392a2268a2596a7a4ac458658cfe64
Ramyashree <ramyashree@example.com>
2025-11-09 13:11:05 +0530

    minor changes

--- ELP_App/elp-app-node/api/routes/user.js
+++ ELP_App/elp-app-node/api/routes/user.js
@@ -5,32 +5,42 @@ const db = require('../../dbconfig.js'); // Import the connection pool
 router.post("/session", (req, res, next) => {
   const userid = req.body.userid;
   const inputpassword = req.body.password;
-  db.query('SELECT * FROM users WHERE user_id = ?', [userid], (error, results) => {
-    if (error) {
-      console.error('Error executing parameterized query:', error);
-      return;
-    }
-    var result = null;
-    if (results.length > 0) {
-      result = results[0];
-    }
-    console.log('User:', result);
-    if ((result !== null) && (inputpassword == result.password)) {
-      user_role = result.role;
-      const randomText = generateRandomText(10);
-      user_token = userid + randomText;
-      insertNewUserToken(userid, user_token);
-      res.status(200).json({
-        message: "Login successful",
-        token: user_token,
-        userid: req.body.userid,
-        role: user_role
-      });
-    }
-    else {
-      res.status(401).json();
-    }
-  });
+  db.query(`SELECT 
+      users.user_id,
+      users.password,
+      users.role,
+      user_names.name
+    FROM users
+    JOIN user_names on users.user_id = user_names.user_id
+    WHERE users.user_id = ?`,
+    [userid], (error, results) => {
+      if (error) {
+        console.error('Error executing parameterized query:', error);
+        return;
+      }
+      var result = null;
+      if (results.length > 0) {
+        result = results[0];
+      }
+      console.log('User:', result);
+      if ((result !== null) && (inputpassword == result.password)) {
+        user_role = result.role;
+        uname = result.name;
+        const randomText = generateRandomText(10);
+        user_token = userid + randomText;
+        insertNewUserToken(userid, user_token);
+        res.status(200).json({
+          message: "Login successful",
+          token: user_token,
+          userid: req.body.userid,
+          role: user_role,
+          name: uname
+        });
+      }
+      else {
+        res.status(401).json();
+      }
+    });
 });
 
 router.delete("/session/current", (req, res, next) => {
